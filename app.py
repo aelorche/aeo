316a986e0c959cf3069b2622348e4bd52ac879a0
aelorche <ahmed.elorche@gmail.com>
2024-03-22 14:12:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask, request, jsonify
-from dotenv import load_dotenv
 import os
-from flask_cors import CORS
-
-load_dotenv()
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-CORS(app)
 
-# Chargez les variables d'environnement
+# Load environment variables
 app.config['DEBUG'] = os.getenv('FLASK_DEBUG') == 'True'
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def hello_name():
+    if request.headers['Content-Type'] != 'application/json; charset=utf-8':
+        return jsonify({'error': 'Unsupported Media Type'}), 415
+
     data = request.json
     if data is None:
         return jsonify({'error': 'No JSON data received'}), 400
@@ -20,13 +18,13 @@ def hello_name():
     name = data.get('input', 'World')
     number_1 = data.get('number_1', '1')
     number_2 = data.get('number_2', '2')
-    
+
     try:
         number_3 = int(number_1) + int(number_2)
     except ValueError:
         return jsonify({'error': 'Invalid number format'}), 400
 
-    response = {'message': f'Hello, {name}!', 'number_1 + number_2':  f'{number_3}'}
+    response = {'message': f'Hello, {name}!', 'number_1 + number_2': f'{number_3}'}
     return jsonify(response)
 
 if __name__ == '__main__':

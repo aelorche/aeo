d00e171a8688bf3d27c0f3efd33e67ad44215b87
aelorche <ahmed.elorche@gmail.com>
2024-03-22 14:25:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
-import os
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Load environment variables
-app.config['DEBUG'] = os.getenv('FLASK_DEBUG') == 'True'
+@app.route('/')
+def hello_world():
+    # Your data
+    data = {'message': 'Hello, world!'}
 
-@app.route('/', methods=['GET'])
-def hello_name():
-    if request.headers['Content-Type'] != 'application/json;charset=utf-8':
-        return jsonify({'error': 'Unsupported Media Type'}), 415
+    # Setting Content-Type header to 'application/json'
+    response = jsonify(data)
+    response.headers['Content-Type'] = 'application/json'
 
-    data = request.json
-    if data is None:
-        return jsonify({'error': 'No JSON data received'}), 400
-
-    name = data.get('input', 'World')
-    number_1 = data.get('number_1', '1')
-    number_2 = data.get('number_2', '2')
-
-    try:
-        number_3 = int(number_1) + int(number_2)
-    except ValueError:
-        return jsonify({'error': 'Invalid number format'}), 400
-
-    response = {'message': f'Hello, {name}!', 'number_1 + number_2': f'{number_3}'}
-    return jsonify(response)
+    return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context='adhoc')

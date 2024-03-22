8f1f18e6ddad6165125ee2edc51461aa9b78eed9
aelorche <ahmed.elorche@gmail.com>
2024-03-22 10:54:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,21 @@ CORS(app)
 # Chargez les variables d'environnement
 app.config['DEBUG'] = os.getenv('FLASK_DEBUG') == 'True'
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_name():
     data = request.json
+    if data is None:
+        return jsonify({'error': 'No JSON data received'}), 400
+
     name = data.get('input', 'World')
     number_1 = data.get('number_1', '1')
     number_2 = data.get('number_2', '2')
-    number_3 = int(number_1) + int(number_2)
+    
+    try:
+        number_3 = int(number_1) + int(number_2)
+    except ValueError:
+        return jsonify({'error': 'Invalid number format'}), 400
+
     response = {'message': f'Hello, {name}!', 'number_1 + number_2':  f'{number_3}'}
     return jsonify(response)
 

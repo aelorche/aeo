07f29e2f59324c1ef4b5b4fcbdc432964a2dd520
aelorche <ahmed.elorche@gmail.com>
2024-04-04 10:07:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+import io
+import sys
+import numpy as np
+
+app = Flask(__name__)
+
 @app.route('/')
 def aeo_function():
     firstname = request.args.get('firstname', 'World')
     familyname = request.args.get('familyname', 'World')
     date_time = datetime.now()
 
-    # Redirect stdout to a StringIO object to capture the print output
     stdout_backup = sys.stdout
     sys.stdout = io.StringIO()
 
     try:
-        # Execute the code passed as a string
         exec(firstname)
         status = sys.stdout.getvalue()
     except Exception as e:
         status = str(e)
     finally:
-        # Restore stdout
         sys.stdout = stdout_backup
 
     data = {'message': f'Hello {firstname} {familyname}!',
@@ -26,3 +31,6 @@ def aeo_function():
     response.headers['Content-Type'] = 'application/json'
 
     return response
+
+if __name__ == '__main__':
+    app.run(ssl_context='adhoc')
